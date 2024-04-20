163a761ac264b3f43b162a17147bf6bd19b5190e
Eduardo126 <110252112+Eduardo126@users.noreply.github.com>
2024-04-19 21:31:26 -0600

    si

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Acerca de nosotros</h1>
+    <p>Información del grupo de trabajo:</p>
+    <ul>
+        <li>Fernando Jose Cruz Chevez. SMSS141122</li>
+        <li>Luis Angel Escobar Constanza. SMSS</li>
+        <LI>Eduardo Vladimir Hernandez Vargas. SMSS126822</li>
+        <!-- Agrega más nombres y carnés según sea necesario -->
+    </ul>
+    <a href="/">Volver a la página principal</a>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/pe.css') }}">
+    <title>Document</title>
+</head>
+<body>
+<h1>Bienvenido a nuestra aplicación Cita</h1>
+    <p>Con nuestra aplicación, puedes buscar, reservar y gestionar citas con una amplia variedad de profesionales de la salud, desde médicos generales hasta especialistas, en función de tus necesidades específicas. Olvídate de las largas esperas en la sala de espera y la frustración de intentar comunicarte por teléfono para concertar una cita; con solo unos pocos clics en tu dispositivo móvil, tendrás acceso a una red de proveedores de atención médica de confianza</p>
+    <img src="{{ asset('images/Medic.jpg') }}" alt="Descripción de la imagen">
+    <a href="/about">Acerca de nosotros</a>
+    <p>Somos una Clinica medica, especialmente les ofrecemos nuestro servicios medicos a todos nuestros pasientes que lo sea necesario para prepara a nuestro equipo persona para brindales nuestos apoyo.</p>
+<h2>Explicacion sobre nuestro proyecto</h2>
+    <p>Nuestro Proyecto tratara sobre un pagina donde vamos a poder registrar a nuestro pacientes que lleguen a consultorio y tener registro de ellos y trata de mejorar nuestro sitio para tener mas organizado nuestro pacientes para agendar sus citas y tener sus expediente y no estará registrando nuestros datos.
+Aparte vamos a tener un aparado para agendar citas medias para tales fecha y horas para que nuestro paciente que tengo una organización mas cómoda.
+</p>
+</body>
+</html>
